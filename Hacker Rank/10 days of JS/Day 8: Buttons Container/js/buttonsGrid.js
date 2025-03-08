a45f7740e42d0009d2872f209ed8e182d2a59171
Hanzala Khan <pythondeveloper360@gmail.com>
2025-03-09 01:11:04 +0500

    Completed Day 9 of JS

---
+++ Hacker Rank/10 days of JS/Day 8: Buttons Container/js/buttonsGrid.js
@@ -0,0 +1,23 @@
+function rotate(){
+    [
+        document.getElementById('btn2').innerHTML,
+        document.getElementById('btn3').innerHTML,
+        document.getElementById('btn6').innerHTML,
+        document.getElementById('btn9').innerHTML,
+        document.getElementById('btn8').innerHTML,
+        document.getElementById('btn7').innerHTML,
+        document.getElementById('btn4').innerHTML,
+        document.getElementById('btn1').innerHTML,
+    ]   
+    =[
+        document.getElementById('btn1').innerHTML,
+        document.getElementById('btn2').innerHTML,
+        document.getElementById('btn3').innerHTML,
+        document.getElementById('btn6').innerHTML,
+        document.getElementById('btn9').innerHTML,
+        document.getElementById('btn8').innerHTML,
+        document.getElementById('btn7').innerHTML,
+        document.getElementById('btn4').innerHTML,
+
+    ]
+}
