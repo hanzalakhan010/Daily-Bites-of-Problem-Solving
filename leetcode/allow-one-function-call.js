caf261ab4cadc6269c411caa2f823970370496f8
Hanzala Khan <pythondeveloper360@gmail.com>
2025-03-29 07:43:44 +0500

    allow one function call problem solved

---
+++ leetcode/allow-one-function-call.js
@@ -0,0 +1,18 @@
+
+var once = function(fn) {
+    counter = 0
+    return function(...args){
+        if (counter ==0){
+            counter ++
+            return fn(...args)
+        }
+        
+    }
+};
+
+let fn = (a,b,c) => (a + b + c)
+
+let onceFn = once(fn)
+console.log(onceFn(1,2,3)); // 6
+console.log(onceFn(2,3,6)); // returns undefined without calling fn
+
